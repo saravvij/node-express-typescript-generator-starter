dda44c48c174841968334b70727e7368001b0fe0
Saravanan Vijayappan <saravanan.vijayappan@anaplan.com>
2019-05-13 14:51:34 -0700

    init

---
+++ build/components/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const AuthComponent = require("./Auth");
+exports.AuthComponent = AuthComponent;
+const UserComponent = require("./User");
+exports.UserComponent = UserComponent;
+//# sourceMappingURL=index.js.map
